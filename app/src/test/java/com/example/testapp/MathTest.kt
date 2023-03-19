0070683273239452e6aa6c7cf3f34dff90bd2a5e
Yasnovsky <andrey5.1997@mail.ru>
2023-03-19 20:06:30 +0500

    addTest function

---
+++ app/src/test/java/com/example/testapp/MathTest.kt
@@ -0,0 +1,11 @@
+package com.example.testapp
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class MathTest {
+    @Test
+    fun addTest() {
+        assertEquals(2,1 + 1)
+    }
+}
